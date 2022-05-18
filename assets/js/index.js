12d69a77f1f208a7abbc425b855753b3dfe2792b
Ariok-David-LM <ariokdavidl@gmail.com>
2022-05-18 00:09:04 -0500

    feat: Se crea el elemento card desde JS y ya se consulta la API

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,3 +18,49 @@ exampleModal.addEventListener('show.bs.modal', (event) => {
   habilidades.textContent = "Habilidades: " + habilidad
   ataqueYDefensa.textContent = "Ataque:" + atack + "    Defensa:" + defense
 })
+
+const contenedor = document.querySelector('#contenedor')
+
+const renderPokeCard = () => {
+    const newButton = document.createElement('button')
+    const newImg = document.createElement('img')
+    newButton.classList.add('btn')
+    newButton.classList.add('btn-primary')
+    newButton.classList.add('me-3')
+    newButton.setAttribute('type', 'button')
+    newButton.setAttribute('data-bs-toggle','modal')
+    newButton.setAttribute('data-bs-target','#exampleModal')
+    newButton.setAttribute('data-name','cat')
+    newButton.setAttribute('data-id','2')
+    newButton.setAttribute('data-tipo','felinos')
+    newButton.setAttribute('data-habi','correre')
+    newButton.setAttribute('data-atack','152')
+    newButton.setAttribute('data-defense','601')
+    newImg.setAttribute('src','https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg')
+    newImg.setAttribute('alt','imagem')
+    newImg.setAttribute('width','40')
+    newImg.setAttribute('height','40')
+    newButton.innerHTML = 'pokemonsito  '
+    newButton.appendChild(newImg)
+    contenedor.appendChild(newButton)
+}
+ renderPokeCard()
+
+
+
+
+
+
+const main = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(response => response.json())
+        .then((pokemones) => {
+            console.log(pokemones)
+            pokemones.results.forEach(element => {
+                fetch(`${element.url}`)
+                .then((response) => response.json())
+                .then((pokemon) => console.log(pokemon))
+            });
+        })
+}
+ main()
